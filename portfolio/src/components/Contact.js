a60bef41167efa2920965f2f38110dd8b3f360f7
Hanlynui <hanyu.lin59@gmail.com>
2023-06-02 20:30:13 -0400

    fixed submit bug and auto refresh

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,57 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import Section from "./Section";
 
 const Contact = () => {
-  //todo add a bot checker here, i want to make a contact section that will send me an email, also put my linkedin and other contact info up, maybe have my phone number and email to show after users settle a bot check
+  const [query, setQuery] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
 
-  //todo fix clearing message on submit
-  //todo also save state for light and dark mode
+  const handleParam = () => (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuery((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  //todo add a bot checker here,
+  // todo change alert to i want to make a contact section that will send me an email, also put my linkedin and other contact info up, maybe have my phone number and email to show after users settle a bot check
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    Object.entries(query).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    fetch("https://getform.io/f/985c5780-91ab-4b36-a43e-ebbc640e523b", {
+      method: "POST",
+      body: formData,
+    }).then(() => {
+      setQuery({ name: "", phone: "", email: "", message: "" });
+      alert("Contact Form Submitted :)");
+    });
+  };
+
   return (
     <Section title="Contact 📱" subtitle="Let's get in touch!">
-      <div className="p-8 text-center w-full mx-auto md:max-w-lg lg:max-w-xl">
-        <form
-          action="https://getform.io/f/985c5780-91ab-4b36-a43e-ebbc640e523b"
-          method="POST"
-        >
+      <div className="p-2 text-center w-full mx-auto md:max-w-lg lg:max-w-xl">
+        <form method="POST" onSubmit={formSubmit}>
           <div className="grid gap-4">
             <div className="flex flex-col items-center">
-              <label className="capitalize text-sm py-2 font-extralight">
+              <label className="capitalize text-med py-2 font-extralight">
                 Name
               </label>
               <input
                 type="text"
                 name="name"
                 className="w-full border-2 rounded-lg p-3 focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"
+                required
+                value={query.name}
+                onChange={handleParam()}
               ></input>
             </div>
             <div className="flex flex-col items-center">
-              <label className="capitalize text-sm py-2 font-extralight">
+              <label className="capitalize text-med py-2 font-extralight">
                 Phone
               </label>
               <input
                 type="text"
                 name="phone"
                 className="w-full border-2 rounded-lg p-3 focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"
+                required
+                value={query.phone}
+                onChange={handleParam()}
               ></input>
             </div>
             <div className="flex flex-col items-center">
-              <label className="capitalize text-sm py-2 font-extralight">
+              <label className="capitalize text-med py-2 font-extralight">
                 Email
               </label>
               <input
                 type="text"
                 name="email"
                 className="w-full border-2 rounded-lg p-3 focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"
+                required
+                value={query.email}
+                onChange={handleParam()}
               ></input>
             </div>
             <div className="flex flex-col items-center">
-              <label className="capitalize text-sm py-2 font-extralight">
+              <label className="capitalize text-med py-2 font-extralight">
                 Message
               </label>
               <textarea
                 type="text"
                 name="message"
                 className="w-full border-2 rounded-lg p-3 focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white"
+                required
+                value={query.message}
+                onChange={handleParam()}
               ></textarea>
             </div>
             <div className="flex justify-center">
