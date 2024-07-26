aae94ba9ea26fdbf87a5eb06282a443ada1edddc
Your Name <your.email@example.com>
2024-07-25 20:09:32 -0400

    added htaccess:

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,23 +4,31 @@ import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Stack from "./components/Stack";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Redirect from "./components/Redirect";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
+  const location = useLocation();
 
   return (
     <div>
       <div className={darkMode ? "dark" : ""}>
         <Header darkMode={darkMode} setDarkMode={setDarkMode} />
 
-        <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-          <Intro />
-          <Stack />
-          <Projects />
-          <Contact />
-        </div>
+        {location.pathname !== "/redirect" && (
+          <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+            <Intro />
+            <Stack />
+            <Projects />
+            <Contact />
+          </div>
+        )}
         <Routes>
           <Route path="/redirect" element={<Redirect />} />
         </Routes>
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
